dea46522a590134a9fb803bae1b250f80a72a68d
Stainpy <omarashrafzaki@gmail.com>
2021-06-06 08:22:06 +0200

    Update results.py

--- results.py
+++ results.py
@@ -7,10 +7,8 @@ from os import path
 class times:
     timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
 
-
 #Write to text
 class results:
-    #All hits (Contains Secured & Unsecured)   
     def Hits(self):
         results_path = f"./Results/{times.timestamp}"
         file_name = "Hits.txt"
@@ -18,8 +16,7 @@ class results:
             os.makedirs(results_path)
         file_path = path.join(results_path,file_name)
         with open(file_path,"a",errors="ingore") as f:
-            f.write(self + "\n")
-    #Free accounts    
+            f.write(self + "\n") 
     def Free(self):
         results_path = f"./Results/{times.timestamp}"
         file_name = "Free.txt"
@@ -28,7 +25,6 @@ class results:
         file_path = path.join(results_path,file_name)
         with open(file_path,"a",errors="ingore") as f:
             f.write(self + "\n")
-    #Secured accounts
     def Secured(self):
         results_path = f"./Results/{times.timestamp}"
         file_name = "Secured.txt"
@@ -37,7 +33,7 @@ class results:
         file_path = path.join(results_path,file_name)
         with open(file_path,"a",errors="ingore") as f:
             f.write(self + "\n")
-    #Unsecured accounts
+
     def Unsecured(self):
         results_path = f"./Results/{times.timestamp}"
         file_name = "Unsecured.txt"
