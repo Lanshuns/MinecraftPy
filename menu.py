0471a8ba708bf97b0910c1aa7680cd71abffba7c
Stainpy <omarashrafzaki@gmail.com>
2021-06-06 08:20:29 +0200

    Update menu.py
    updated v3

--- menu.py
+++ menu.py
@@ -8,41 +8,42 @@ class colors:
     yellow = "\u001b[33;1m" + "\033[1m"
     red = "\u001b[31;1m" + "\033[1m"
     normal = "\u001b[0m" + "\033[1m"
+    cyan = "\u001b[36m" + "\033[1m"
+    magenta = "\u001b[35m" + "\033[1m"
+    blue = "\u001B[34m" + "\033[1m"
 
 
 #Title & main menu & about
 class main_menu:
-    os.system("mode con cols=140 lines=30")
-    os.system("title Minecraft Py v2.0")
+    # os.system("mode con cols=153 lines=31")
+    os.system("title Minecraft Py v3.0")
 
     about = """
-    My Discord: StaiN#9677
+    Discord: StaiN#9677
 
-    My GitHub: https://github.com/staain
+    GitHub: https://github.com/Stainpy
 
-    My Bitcoin Address: 3JT4WFGqqrwvzN5hZzEXtC1pbjV9cMvZSn
+    Bitcoin Address: 3JT4WFGqqrwvzN5hZzEXtC1pbjV9cMvZSn
 
-    Awesome Python Master: https://github.com/Categorically
+    Awesome Python Developer: https://github.com/Categorically
     """
 
-    print(colors.normal + """
+    print("""
 
-                    ███╗   ███╗██╗███╗   ██╗███████╗ ██████╗██████╗  █████╗ ███████╗████████╗    ██████╗ ██╗   ██╗
-                    ████╗ ████║██║████╗  ██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝    ██╔══██╗╚██╗ ██╔╝
-                    ██╔████╔██║██║██╔██╗ ██║█████╗  ██║     ██████╔╝███████║█████╗     ██║       ██████╔╝ ╚████╔╝ 
-                    ██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██║     ██╔══██╗██╔══██║██╔══╝     ██║       ██╔═══╝   ╚██╔╝  
-                    ██║ ╚═╝ ██║██║██║ ╚████║███████╗╚██████╗██║  ██║██║  ██║██║        ██║       ██║        ██║   
-                    ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝        ╚═╝       ╚═╝        ╚═╝   
-
-    """)
+            \u001b[0m\033[1m███╗   ███╗██╗███╗   ██╗███████╗ ██████╗██████╗  █████╗ ███████╗████████╗    \u001B[34m██████╗ \u001b[33;1m██╗   ██╗
+            \u001b[0m\033[1m████╗ ████║██║████╗  ██║██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝    \u001B[34m██╔══██╗\u001b[33;1m╚██╗ ██╔╝
+            \u001b[0m\033[1m██╔████╔██║██║██╔██╗ ██║█████╗  ██║     ██████╔╝███████║█████╗     ██║       \u001B[34m██████╔╝ \u001b[33;1m╚████╔╝ 
+            \u001b[0m\033[1m██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██║     ██╔══██╗██╔══██║██╔══╝     ██║       \u001B[34m██╔═══╝   \u001b[33;1m╚██╔╝  
+            \u001b[0m\033[1m██║ ╚═╝ ██║██║██║ ╚████║███████╗╚██████╗██║  ██║██║  ██║██║        ██║       \u001B[34m██║        \u001b[33;1m██║   
+            \u001b[0m\033[1m╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝        ╚═╝       \u001B[34m╚═╝        \u001b[33;1m╚═╝    
+        """)
 
     easygui.msgbox(msg=about, title='About', ok_button='Close', image=None, root=None)
 
 
-#Threads input
 def threads():
     while 1:
-        num_threads = (input("  Threads: ")).lstrip('0')
+        num_threads = (input(colors.normal + "  Threads: ")) 
         if num_threads.isdigit():
             num_threads = int(num_threads)
             if num_threads == 0:
@@ -55,10 +56,9 @@ def threads():
         else:
             easygui.msgbox(msg="Error!! Please enter a digit.", title='Error', ok_button='OK', image=None, root=None)
 
-#Timeout input
 def timeout():
     while 1:
-        num_timeout = (input("  Timeout(Secs): ")).lstrip('0')
+        num_timeout = (input("  Timeout(Secs): ")) 
         if num_timeout.isdigit():
             num_timeout = int(num_timeout)
             return num_timeout
@@ -66,10 +66,9 @@ def timeout():
         else:
             easygui.msgbox(msg="Error!! Please enter a digit.", title='Error', ok_button='OK', image=None, root=None)
 
-#Proxy Type input
 def proxy_type():
     while 1:
-        proxytype_input = (input("  Proxies Type:\n  [1]: Http/s\n  [2]: Socks4\n  [3]: Socks5\n  > ")).lstrip('0')
+        proxytype_input = (input("  Proxies Type:\n  [1]: Http/s\n  [2]: Socks4\n  [3]: Socks5\n  > ")) 
         if proxytype_input.isdigit():
             proxytype_input = int(proxytype_input)
             if proxytype_input == 1:
@@ -85,10 +84,9 @@ def proxy_type():
         else:
             easygui.msgbox(msg="Error!! Please enter a digit.", title='Error', ok_button='OK', image=None, root=None)
 
-#Show Logs input
 def logs():
     while 1:
-        Show_Logs = (input("  Show Logs:\n  [1]: Yes\n  [2]: No\n  > ")).lstrip('0')
+        Show_Logs = (input("  Show Logs:\n  [1]: Yes\n  [2]: No\n  > ")) 
         if Show_Logs.isdigit():
             Show_Logs = int(Show_Logs)
             if Show_Logs <= 2:
