6b28f105da46916b4bf1847318ca51aa21825217
Stainpy <omarashrafzaki@gmail.com>
2021-06-06 08:20:56 +0200

    Update proxies.py
    updated v3

--- proxies.py
+++ proxies.py
@@ -1,27 +1,6 @@
-import easygui
-
-
-#Proxies
-def requests_proxy(proxy,proxy_type):
-    proxy_parts = proxy.split(":")
-    #No auth
-    if len(proxy_parts) == 2:
-        proxy = {"http": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}",
-                 "https": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}"}
-        return proxy
-    #Auth
-    elif len(proxy_parts) == 4:
-        proxy = {"http": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}",
-                 "https": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}"}
-        return proxy
-    else:
-        pass
-
-
 #Load Proxies
 def load_proxy():
-    loadproxy = easygui.fileopenbox(msg="Load Your Proxies", title="Proxies", default='*', filetypes=["*.txt"], multiple=False)
-    with open(loadproxy,"r",encoding="utf8",errors="replace") as proxy_file:
+    with open("proxies.txt","r",encoding="utf8",errors="replace") as proxy_file:
         proxy_text = proxy_file.read()
         proxies = proxy_text.splitlines()
 
@@ -30,3 +9,20 @@ def load_proxy():
         proxiescount = len(proxies)
         print(f"  Loaded Proxies: {proxiescount} \n \n  Checking... \n \n")
         return proxies
+
+
+#Proxies
+def requests_proxy(proxy,proxy_type):
+    proxy_parts = proxy.split(":")
+    #No auth
+    if len(proxy_parts) == 2:
+        proxy = {"http": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}",
+                "https": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}"}
+        return proxy
+    #Auth
+    elif len(proxy_parts) == 4:
+        proxy = {"http": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}",
+                "https": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}"}
+        return proxy
+    else:
+        pass
