ee933c5b3305ee46a1c19e37deedc8f0022c5b45
Stainpy <omarashrafzaki@gmail.com>
2021-06-24 02:06:17 +0200

    update for 3.2

--- static/results.py
+++ static/results.py
@@ -3,15 +3,12 @@ import os
 from os import path
 
 
-#TimeSamp
-class times:
-    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
 
-
-#Write to text
 class results:
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+    
     def Hits(self):
-        results_path = f"./Results/{times.timestamp}"
+        results_path = f"./Results/{results.timestamp}"
         file_name = "Hits.txt"
         if not os.path.exists(results_path):
             os.makedirs(results_path)
@@ -19,7 +16,7 @@ class results:
         with open(file_path,"a",errors="ingore") as f:
             f.write(self + "\n") 
     def Free(self):
-        results_path = f"./Results/{times.timestamp}"
+        results_path = f"./Results/{results.timestamp}"
         file_name = "Free.txt"
         if not os.path.exists(results_path):
             os.makedirs(results_path)
@@ -27,7 +24,7 @@ class results:
         with open(file_path,"a",errors="ingore") as f:
             f.write(self + "\n")
     def Secured(self):
-        results_path = f"./Results/{times.timestamp}"
+        results_path = f"./Results/{results.timestamp}"
         file_name = "Secured.txt"
         if not os.path.exists(results_path):
             os.makedirs(results_path)
@@ -35,7 +32,7 @@ class results:
         with open(file_path,"a",errors="ingore") as f:
             f.write(self + "\n")
     def Unsecured(self):
-        results_path = f"./Results/{times.timestamp}"
+        results_path = f"./Results/{results.timestamp}"
         file_name = "Unsecured.txt"
         if not os.path.exists(results_path):
             os.makedirs(results_path)
