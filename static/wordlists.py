6dd3db0eddcea8dc4d40be36eff198154fe00cf4
Stainpy <omarashrafzaki@gmail.com>
2021-06-24 02:07:09 +0200

    update for v3.2

--- static/wordlists.py
+++ static/wordlists.py
@@ -1,30 +1,26 @@
 from multiprocessing import JoinableQueue as Queue
 
 
-#Load text and replace errors with "spaces", wthout using error kwarg it will cause an exception
+
 def load_wordlist():
     with open("wordlist.txt","r",encoding="utf8",errors="replace") as wordlist_file:
         wordlist_text = wordlist_file.read()
         wordlists = wordlist_text.splitlines()
         
-        #Remove dupes
         wordlists = list(dict.fromkeys(wordlists))
         wordlist_count = len(wordlists)
         print(f"  Loaded Wordlists: {wordlist_count}")
         return wordlists
 
 
-#Loop the wordlists
 def vail_wordlists(wordlists):
     q = Queue(maxsize=0)
     for wordlist in wordlists:
         valid_wordlist = False 
         try:
-            #Only split the first instance of ":"
             username,password = wordlist.split(":",1)
             valid_wordlist = True
         except:
-            #Some wordlists will not be valid and cause an exception if they can't be split or unpacked
             pass
         if valid_wordlist:
             q.put(wordlist.split(":",1))
