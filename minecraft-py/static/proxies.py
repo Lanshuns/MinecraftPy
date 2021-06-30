2c5bf5d5d6e6fbd8ae76e496084ec514b94bb700
Stainpy <omarashrafzaki@gmail.com>
2021-06-30 20:25:38 +0200

    v3.5

--- minecraft-py/static/proxies.py
+++ minecraft-py/static/proxies.py
@@ -1,24 +1,39 @@
+from static.menu import colors
+import time
 
-def load_proxy():
-    with open("proxies.txt","r",encoding="utf8",errors="replace") as proxy_file:
-        proxy_text = proxy_file.read()
-        proxies = proxy_text.splitlines()
 
-        proxies = list(dict.fromkeys(proxies))
-        proxiescount = len(proxies)
-        print(f"  Loaded Proxies: {proxiescount}")
-        return proxies
+
+def load_proxy(proxy_type):
+    if proxy_type == 'proxyless':
+        pass
+    else:
+        with open("proxies.txt","r+",encoding="utf8",errors="replace") as proxy_file:
+            proxy_text = proxy_file.read()
+            proxies = proxy_text.splitlines()
+
+            proxies = list(dict.fromkeys(proxies))
+            proxiescount = len(proxies)
+            if proxiescount == 0:
+                print(colors.red + "  Error!! No proxies found.")
+                time.sleep(3)
+                quit()
+            else:
+                print(f"  Loaded Proxies: {proxiescount}")
+                return proxies
 
 
 def requests_proxy(proxy,proxy_type):
-    proxy_parts = proxy.split(":")
-    if len(proxy_parts) == 2:
-        proxy = {"http": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}",
-                "https": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}"}
-        return proxy
-    elif len(proxy_parts) == 4:
-        proxy = {"http": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}",
-                "https": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}"}
-        return proxy
-    else:
+    if proxy_type == 'proxyless':
         pass
+    else:
+        proxy_parts = proxy.split(":")
+        if len(proxy_parts) == 2:
+            proxy = {"http": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}",
+                    "https": f"{proxy_type}://{proxy_parts[0]}:{proxy_parts[1]}"}
+            return proxy
+        elif len(proxy_parts) == 4:
+            proxy = {"http": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}",
+                    "https": f"{proxy_type}://{proxy_parts[2]}:{proxy_parts[3]}@{proxy_parts[0]}:{proxy_parts[1]}"}
+            return proxy
+        else:
+            pass
