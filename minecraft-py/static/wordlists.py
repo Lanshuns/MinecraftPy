b768f1b7ca499917e4cdb93171ed1a8de90dede1
Stainpy <omarashrafzaki@gmail.com>
2021-06-30 20:26:37 +0200

    v3.5

--- minecraft-py/static/wordlists.py
+++ minecraft-py/static/wordlists.py
@@ -1,16 +1,23 @@
 from multiprocessing import JoinableQueue as Queue
+from static.menu import colors
+import time
 
 
 
 def load_wordlist():
-    with open("wordlist.txt","r",encoding="utf8",errors="replace") as wordlist_file:
+    with open("wordlists.txt","r",encoding="utf8",errors="replace") as wordlist_file:
         wordlist_text = wordlist_file.read()
         wordlists = wordlist_text.splitlines()
         
         wordlists = list(dict.fromkeys(wordlists))
         wordlist_count = len(wordlists)
-        print(f"  Loaded Wordlists: {wordlist_count}")
-        return wordlists
+        if wordlist_count == 0:
+            print(colors.red + "  Error!! No wordlists found.")
+            time.sleep(3)
+            quit()
+        else:
+            print(f"\n  Loaded Wordlists: {wordlist_count}")
+            return wordlists
 
 
 def vail_wordlists(wordlists):
