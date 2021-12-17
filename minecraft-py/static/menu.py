4b8e31de14b5b46e1884ceded618e65199fdc11d
Stainpy <omarashrafzaki@gmail.com>
2021-12-18 01:21:03 +0200

    Update menu.py

--- minecraft-py/static/menu.py
+++ minecraft-py/static/menu.py
@@ -12,8 +12,6 @@ class colors:
 
 
 def main_menu():
-    os.system("title Minecraft Py Checker")
-
     title = print("""
 
             \u001b[0m\033[1m███╗   ███╗██╗███╗   ██╗███████╗ ██████╗██████╗  █████╗ ███████╗████████╗    \u001B[34m██████╗ \u001b[33;1m██╗   ██╗
@@ -27,7 +25,7 @@ def main_menu():
 
 def proxy_type():
     while 1:
-        proxytype_input = (input(colors.normal + "  Proxies Type:\n  [1]: Http/s\n  [2]: Socks4\n  [3]: Socks5\n  > ")) 
+        proxytype_input = (input(colors.normal + " [1]: Http/s\n [2]: Socks4\n [3]: Socks5\n > ")) 
         if proxytype_input.isdigit():
             proxytype_input = int(proxytype_input)
             if proxytype_input == 1:
@@ -37,13 +35,13 @@ def proxy_type():
             elif proxytype_input == 3:
                 return 'socks5'
             else:
-                print(colors.red + "  Error!! Please choose one of available modes.")
+                print(colors.red + " Error!! Please choose one of available modes.")
         else:
-            print(colors.red + "  Error!! Please enter a digit.")
+            print(colors.red + " Error!! Please enter a digit.")
 
 def timeout():
     while 1:
-        num_timeout = (input(colors.normal + "  Timeout(Secs): ")) 
+        num_timeout = (input(colors.normal + " Timeout(Secs): ")) 
         if num_timeout.isdigit():
             num_timeout = int(num_timeout)
             if num_timeout == 0:
@@ -51,32 +49,32 @@ def timeout():
             return num_timeout
 
         else:
-            print(colors.red + "  Error!! Please enter a digit.")
+            print(colors.red + " Error!! Please enter a digit.")
 
 def threads():
     while 1:
-        num_threads = (input(colors.normal + "  Threads: ")) 
+        num_threads = (input(colors.normal + " Threads: ")) 
         if num_threads.isdigit():
             num_threads = int(num_threads)
             if num_threads == 0:
-                print(colors.red + "Wrong Threads Number!! Please choose from 1 to 200.")
-            elif num_threads <= 200:
+                print(colors.red + " Wrong Threads Number!! Please choose from 1 to 200.")
+            elif num_threads <= 1000:
                 return num_threads
             else:
-                print(colors.red + "  Wrong Threads Number!! Max threads is 200.")
+                print(colors.red + " Wrong Threads Number!! Max threads is 200.")
         else:
-            print(colors.red + "  Error!! Please enter a digit.")
+            print(colors.red + " Error!! Please enter a digit.")
 
 def logs():
     while 1:
-        Show_Logs = (input(colors.normal + "  Show Logs:\n  [1]: Yes\n  [2]: No\n  > ")) 
+        Show_Logs = (input(colors.normal + " Show Logs:\n [1]: Yes\n [2]: No\n > ")) 
         if Show_Logs.isdigit():
             Show_Logs = int(Show_Logs)
-            if Show_Logs == 0:
-                print(colors.red + "Wrong Threads Number!! Please choose from 1 to 200.")
-            elif Show_Logs <= 2:
+            if Show_Logs == 1:
                 return Show_Logs
+            elif Show_Logs == 2:
+                break
             else:
-                print(colors.red + "  Error!! Please choose one of available modes.")
+                print(colors.red + " Error!! Please choose one of available modes.")
         else:
-            print(colors.red + "  Error!! Please enter a digit.")
+            print(colors.red + " Error!! Please enter a digit.")
